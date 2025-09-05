ac94805be3fd8a385ad1c24c9593494d8607eeeb
Purvansh <purvansh.23bcs10169@ms.sst.scaler.com>
2025-09-06 02:31:37 +0530

    Updated Aboue mt

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,10 +6,10 @@ export default function AboutMe({ id }) {
   return (
       <div className="about-container" id={id}>
         <div className="aboutMe-text">
-            <h3>Hey there, 👋</h3>
-            I'm <span>Purvansh</span>, a passionate <span>photographer</span> who loves capturing moments and exploring creativity beyond the digital world. You can often find me out and about, watching life unfold or engaging in creative pursuits outside the screen. Whether it's through my lens or hands-on artistic projects, I thrive on bringing fresh perspectives to everything I do.
-            <br/> <br/>
-            Want to know more about me? Feel free to <a className='resume' href="https://drive.google.com/drive/folders/1CC-_PmNC08sz2GFfAkkWZguPWKMKxmTr?usp=sharing" target="_blank" rel="noopener noreferrer">explore my resume.</a>
+          <h3>Hey there, 👋</h3>
+          I'm <span>Purvansh</span>, a pre-final year student pursuing <span>BSc in Computer Science</span> with a specialization in <span>AI and ML</span>. I'm a creative photographer who loves to unfold the world around and behind digital screens. My passion lies in blending technology and creativity, whether it's through coding, capturing moments, or exploring new perspectives. Let's connect and create something extraordinary together!
+          <br/> <br/>
+          Want to know more about me? Feel free to <a className='resume' href="https://drive.google.com/drive/folders/1CC-_PmNC08sz2GFfAkkWZguPWKMKxmTr?usp=sharing" target="_blank" rel="noopener noreferrer">explore my resume.</a>
         </div>
         <div className="profile-photo">
           <div className="heading"> About Me</div>
