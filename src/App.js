da4c6989b2d975d361496d6ab19741373ad2b9fe
Purvansh <purvansh.23bcs10169@ms.sst.scaler.com>
2025-12-15 22:15:07 +0530

    feat: Update Spline viewer version, add project section ID, and shorten Spline background fade-in transition.

--- src/App.js
+++ src/App.js
@@ -56,6 +56,7 @@ function App() {
         <HomeBanner id="home" />
 
         <ProjectCard
+          id="project"
           projectTitle="BrainWave"
           className="even"
           projectDesc="Brainwave - Modern UI/UX website, developed using React.js and Tailwind CSS, exemplifies modern UI/UX principles. Its sleek design, seamless animations, and overall user experience set a high standard, serving as a reference or inspiration for future modern applications or websites in general."
